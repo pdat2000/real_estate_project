1cd77fd561619787ee248b04b058e0f48c7865d4
phamdat <phamdat2982k@gmail.com>
2024-03-31 22:51:34 +0700

    crud property type

--- server/controllers/propertyType.js
+++ server/controllers/propertyType.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler")
 const db = require("../models")
-const { Sequelize } = require("sequelize")
+const { Sequelize, or } = require("sequelize")
+const { throwErrorWithStatus } = require("../middlewaves/errorHandler")
 
 const createPropertyType = asyncHandler(async (req, res) => {
   const { name } = req.body
@@ -17,7 +18,7 @@ const createPropertyType = asyncHandler(async (req, res) => {
 })
 
 const getPropertyType = asyncHandler(async (req, res) => {
-  const { limit, page, fields, type, name, ...query } = req.query
+  const { limit, page, fields, name, sort, ...query } = req.query
   const options = {}
   if (fields) {
     const attributes = fields.split(",")
@@ -26,9 +27,7 @@ const getPropertyType = asyncHandler(async (req, res) => {
       options.attributes = {
         exclude: attributes.map((el) => el.replace("-", "")),
       }
-    } else {
-      options.attributes = attributes
-    }
+    } else options.attributes = attributes
   }
 
   if (name)
@@ -37,7 +36,21 @@ const getPropertyType = asyncHandler(async (req, res) => {
       "LIKE",
       `%${name.toLocaleLowerCase()}%`
     )
-  if (type === "ALL") {
+
+  if (sort) {
+    const order = sort
+      .split(",")
+      .map((el) =>
+        el.startsWith("-")
+          ? [el.replace("-", ""), "DESC"]
+          : [el.replace("-", ""), "ASC"]
+      )
+    console.log("sort", sort)
+    console.log("order", order)
+    options.order = order
+  }
+
+  if (!limit) {
     const response = await db.PropertyType.findAll({
       where: query,
       ...options,
@@ -47,9 +60,52 @@ const getPropertyType = asyncHandler(async (req, res) => {
       mes: response.length > 0 ? "Got" : "Cannot get propertyTypes",
       propertyType: response,
     })
-  } else {
-    return res.json({})
   }
+
+  const prevPage = page - 1 <= 0 ? 1 : page
+  const offset = (prevPage - 1) * limit
+  if (offset) options.offset = offset
+  options.limit = +limit
+  const response = await db.PropertyType.findAndCountAll({
+    where: query,
+    ...options,
+  })
+  return res.json({
+    success: response.length > 0,
+    mes: response.length > 0 ? "Got" : "Cannot get propertyTypes",
+    propertyType: response,
+  })
 })
 
-module.exports = { createPropertyType, getPropertyType }
+const updatePropertyType = asyncHandler(async (req, res, next) => {
+  const { id } = req.params
+  if (Object.keys(req.body).length === 0)
+    return throwErrorWithStatus(403, "Need less 1 argument", res, next)
+  const response = await db.PropertyType.update(req.body, {
+    where: { id },
+  })
+
+  return res.json({
+    success: response[0] > 0,
+    mes: response[0] > 0 ? "Updated" : "No data is update",
+  })
+})
+
+const removePropertyType = asyncHandler(async (req, res, next) => {
+  const { id } = req.params
+  const response = await db.PropertyType.destroy({
+    where: { id },
+  })
+
+  return res.json({
+    success: response > 0,
+    mes: response > 0 ? "Deleted" : "No data is deleted",
+  })
+})
+
+module.exports = {
+  createPropertyType,
+  getPropertyType,
+  updatePropertyType,
+  removePropertyType,
+}
--- server/routes/propertyType.js
+++ server/routes/propertyType.js
@@ -3,7 +3,7 @@ const Joi = require("joi")
 const ctrls = require("../controllers/propertyType")
 const validateDto = require("../middlewaves/validation")
 const { isAdmin, verifyToken } = require("../middlewaves/verifyToken")
-const { stringReq } = require("../middlewaves/joiSchema")
+const { stringReq, string } = require("../middlewaves/joiSchema")
 
 router.post(
   "/",
@@ -18,7 +18,20 @@ router.post(
   ),
   ctrls.createPropertyType
 )
-
 router.get("/", ctrls.getPropertyType)
+router.patch(
+  "/:id",
+  verifyToken,
+  isAdmin,
+  validateDto(
+    Joi.object({
+      name: string,
+      description: string,
+      images: string,
+    })
+  ),
+  ctrls.updatePropertyType
+)
+router.delete("/:id", verifyToken, isAdmin, ctrls.removePropertyType)
 
 module.exports = router
