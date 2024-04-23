96efb29e6f37ef974d98572b017737a21c693dfd
phamdat <phamdat2982k@gmail.com>
2024-04-23 22:27:47 +0700

    add next back pagination

--- client/src/components/paginations/Pagination.jsx
+++ client/src/components/paginations/Pagination.jsx
@@ -2,9 +2,17 @@ import usePagination from '~/hooks/usePagination'
 import PaginationItem from './PaginationItem'
 import { Button } from '..'
 import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md'
-import { useSearchParams } from 'react-router-dom'
+import { createSearchParams, useSearchParams } from 'react-router-dom'
+import withRouter from '~/hocs/withRouter'
 
-const Pagination = ({ total, limit, page, sibling = 0 }) => {
+const Pagination = ({
+  total,
+  limit,
+  page,
+  sibling = 0,
+  navigate,
+  location,
+}) => {
   const paginations = usePagination({
     total,
     limit,
@@ -13,9 +21,24 @@ const Pagination = ({ total, limit, page, sibling = 0 }) => {
   })
   const [searchParams] = useSearchParams()
 
+  const handleNextPage = () => {
+    if (page >= Math.ceil(total / limit)) return
+    navigate({
+      pathname: location.pathname,
+      search: createSearchParams({ page: page + 1 }).toString(),
+    })
+  }
+  const handleBackPage = () => {
+    if (page <= 1 || !page) return
+    navigate({
+      pathname: location.pathname,
+      search: createSearchParams({ page: page - 1 }).toString(),
+    })
+  }
+
   return (
     <div className="flex items-center justify-center gap-2">
-      <Button className="bg-main-500">
+      <Button className="bg-main-500" handleOnClick={handleBackPage}>
         <MdArrowBackIos />
       </Button>
       {paginations?.map((el, idx) => (
@@ -25,11 +48,11 @@ const Pagination = ({ total, limit, page, sibling = 0 }) => {
           page={searchParams.get('page')}
         />
       ))}
-      <Button className="bg-main-500">
+      <Button className="bg-main-500" handleOnClick={handleNextPage}>
         <MdArrowForwardIos />
       </Button>
     </div>
   )
 }
 
-export default Pagination
+export default withRouter(Pagination)
